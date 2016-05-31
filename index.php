cd0e65648546c7239f166d8e2466959a9a7658ba
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-30 21:19:18 -0600

    learning java script

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>this is how we do!</title>
+	</head>
+	<body>
+		<h1>time for some learning</h1>
+		<p> lets do it up lets get going and learn this so we can get a job.</p>
+		<ul>
+			<li> testing </li>
+			<li> testing </li>
+			<li> 123 </li>
+		</ul>
+		<script src="myScript.js">
+
+		</script>
+</body>
+</html>
